ff68b9908fbc7b0c30af2ad96f7b0b274c21407d
SoluyanovKonstantin <dr.chtec@yandex.ru>
2020-06-24 14:41:58 +0400

    feat: add instructions in readme.md

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# practice_chirkov_8
-IT-31 Soluyanov
+# Практическая работа №8
+ИТ-31 Солуянов
+Платформа Windows 10 Home v.2004
+
+1) Установить docker desktop [здесь](https://www.docker.com/get-started) и ввести в терминале `docker pull ubuntu` и `docker pull postgres`.
+Для запуска образа нужно создать контейнер и запустить его, для этого можно либо ввести такую команду `winpty docker run -it <image_name>` либо такие `docker container create --name my_container <image_name>` `winpty docker container start -it <name_container>`. Команда run - сразу создает и запускает контейнер, во втором случае эти действия разделены, --name - необязательная опция которая задает имя контейнеру, иначе имя будет задано автоматически, -it - это 2 опции --interactive и --tty, дает возможность сразу после запуска давать команды контейнеру.
+
+2) 
+   - Создаем Dockerfile, он лежит в папке docker с комментариями.
+   - Создаем образ командой `docker build --tag my_image .` --tag/-t - задает тэг для образа.
+   - Cоздаем контейнер командой `docker container create --name my_container my_image`
+   - Запускаем контейнер  `docker container start my_container`
+
+3)
+   - ```winpty docker exec -it my_container bash```
+   - ```psql -h localhost -p 5432 -U postgres -W```
+   - ```CREATE TABLE name_table (ID int primary key,Name text);```
+   - ```insert into name_table values (1, 'Text');```
+   - ```SELECT * FROM name_table;```
+   - ```\q```
+   - ```exit```
+   - Cоздаем дамп ```docker exec -t my_container pg_dumpall -c -U postgres > ./pg-init-scripts/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql```
+
+4) Docker Compose устанавливается вместе с Docker desktop
+
+5)
+   - Перейти на директорию выше
+   - Ввести `docker-compose -f docker-compose.yml up`
+   - В браузере зайти на http://localhost:8080/
+   - Выбрать движок PostgreSQL, имя пользователя postgres, пароль example, база данных postgres
---
+++ docker/pg-init-scripts/dump_24-06-2020_14_02_54.sql
@@ -0,0 +1,267 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Drop databases (except postgres and template1)
+--
+
+DROP DATABASE name;
+
+
+
+
+--
+-- Drop roles
+--
+
+DROP ROLE postgres;
+
+
+--
+-- Roles
+--
+
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'md53175bce1d3201d16594cebf9d7eb3f9d';
+
+
+
+
+
+
+--
+-- Databases
+--
+
+--
+-- Database "template1" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.3 (Debian 12.3-1.pgdg100+1)
+-- Dumped by pg_dump version 12.3 (Debian 12.3-1.pgdg100+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+UPDATE pg_catalog.pg_database SET datistemplate = false WHERE datname = 'template1';
+DROP DATABASE template1;
+--
+-- Name: template1; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
+
+
+ALTER DATABASE template1 OWNER TO postgres;
+
+\connect template1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE template1; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE template1 IS 'default template for new databases';
+
+
+--
+-- Name: template1; Type: DATABASE PROPERTIES; Schema: -; Owner: postgres
+--
+
+ALTER DATABASE template1 IS_TEMPLATE = true;
+
+
+\connect template1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE template1; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE CONNECT,TEMPORARY ON DATABASE template1 FROM PUBLIC;
+GRANT CONNECT ON DATABASE template1 TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "name" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.3 (Debian 12.3-1.pgdg100+1)
+-- Dumped by pg_dump version 12.3 (Debian 12.3-1.pgdg100+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: name; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE name WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
+
+
+ALTER DATABASE name OWNER TO postgres;
+
+\connect name
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "postgres" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.3 (Debian 12.3-1.pgdg100+1)
+-- Dumped by pg_dump version 12.3 (Debian 12.3-1.pgdg100+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE postgres;
+--
+-- Name: postgres; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE postgres WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
+
+
+ALTER DATABASE postgres OWNER TO postgres;
+
+\connect postgres
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE postgres IS 'default administrative connection database';
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: name_table; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.name_table (
+    id integer NOT NULL,
+    name text
+);
+
+
+ALTER TABLE public.name_table OWNER TO postgres;
+
+--
+-- Data for Name: name_table; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.name_table (id, name) FROM stdin;
+1	Text
+\.
+
+
+--
+-- Name: name_table name_table_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.name_table
+    ADD CONSTRAINT name_table_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
